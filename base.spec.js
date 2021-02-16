9d39d6d6505b1db8433a1427a8a0c7f22589d638
Cameron King <cameronking42@gmail.com>
2021-02-16 12:07:52 -0500

    Adds end implemented in terms of anything and not.

--- base.spec.js
+++ base.spec.js
@@ -4,6 +4,7 @@ const Grammar = require('./grammar.js');
 o.spec('base', () => {
     let gr;
     o.beforeEach(() => gr = new Grammar());
+
     o('matches anything', () => {
         gr.anythingWorks = function() {
             if (this.apply('anything')) return true;
@@ -33,4 +34,33 @@ o.spec('base', () => {
             o(match).equals(false)(`anything doesnt match ${msg}`);
         });
     });
+
+    o('matches end', () => {
+        gr.endWorks = function() {
+            if (this.apply('end')) return true;
+            return false;
+        };
+
+        const valid = [
+            ['', 'empty string'],
+            [[], 'empty array'],
+        ];
+
+        valid.forEach(([seed, msg]) => {
+            const match = gr.match(seed, 'endWorks');
+            o(match).equals(true)(`end matches ${msg}`);
+        });
+
+        const invalid = [
+            ['s', 'character'],
+            [['s'], 'array with character'],
+            [[{}], 'array with empty object'],
+            [[[]], 'nested array'],
+        ];
+
+        invalid.forEach(([seed, msg]) => {
+            const match = gr.match(seed, 'endWorks');
+            o(match).equals(false)(`end doesnt match ${msg}`);
+        });
+    })
 })
--- grammar.js
+++ grammar.js
@@ -1,3 +1,5 @@
+const fail = { toString() { return 'match failed' } };
+
 module.exports = class Grammar {
     constructor() {
         this.stream = null;
@@ -7,14 +9,48 @@ module.exports = class Grammar {
     match(stream, startRule) {
         this.stream = stream;
         this.pos = 0;
-        return this.apply(startRule);
+        try {
+            return this.apply(startRule);
+        } catch (e) {
+            if (e === fail) return false;
+            throw e;
+        }
     }
 
     apply(rule) {
         return this[rule]();
     }
 
+    // stream control
+    mark() {
+        return this.pos;
+    }
+
+    reset(pos) {
+        this.pos = pos;
+    }
+
+    // rules
     anything() {
+        if (this.atEndOfStream()) throw fail;
         return this.stream[this.pos++];
+    }
+
+    _not(pred) {
+        const pos = this.mark();
+        try { pred() } catch(e) {
+            if (e !== fail) throw e;
+            this.reset(pos);
+            return true;
+        }
+        throw fail;
+    }
+
+    end() {
+        return this._not(() => this.apply('anything'));
+    }
+
+    atEndOfStream() {
+        return this.pos === this.stream.length;
     }
 }
